86ecab023b4fba662a2d992b20b63494d62c67a3
hldr <haledir@users.noreply.github.com>
2023-05-25 13:49:28 +0200

    Feat/new enemy screen (#3)
    enemy panel rework

--- EnemyPanel.js
+++ EnemyPanel.js
@@ -7,28 +7,34 @@ Vue.component("enemy-panel", {
     },
     template: `
     <div class="enemy-panel gradient-box box-enemy texte" v-bind:class="enemy.slot + ' ' + enemy.active" :style="{ opacity: enemy.value.hp.toString() === '0' ? 0.4 : 1 }">
-        <table class="table-enemy" >
-            <tr>
-                <td colspan="3" rowspan="2" class="cell-image"><img :src="getPkmnImagePath()" width="75px" height="75px" alt="pokemon image"></td>
-                <td colspan="2" class="cell-species cell-center"> Lv: {{ enemy.value.level }}</td>
-                <td colspan="3" class="cell-species cell-center">{{ enemy.value.species }}</td>
-            </tr>
-            <tr>
-                <td class="cell-center cell-atk">{{ enemy.value.attack }}</td>
-                <td class="cell-center cell-def">{{ enemy.value.defense }}</td>
-                <td class="cell-center cell-spa">{{ enemy.value.specialAttack }}</td>
-                <td class="cell-center cell-spd">{{ enemy.value.specialDefense }}</td>
-                <td class="cell-center cell-spe">{{ enemy.value.speed }}</td>
-            </tr>
-            <tr>
-                <td class="cell-center" colspan="4">{{ enemy.value.move1 }}</td>
-                <td class="cell-center" colspan="4">{{ enemy.value.move2.toString() === null ? '-' : enemy.value.move2.toString() }}</td>
-            </tr>
-            <tr>
-                <td class="cell-center" colspan="4">{{ enemy.value.move3.toString() === null ? '-' : enemy.value.move3.toString() }}</td>
-                <td class="cell-center" colspan="4">{{ enemy.value.move4.toString() === null ? '-' : enemy.value.move4.toString() }}</td>
-            </tr>
-        </table>
+        <div class="table-enemy grid-horizontal-container">
+            <div class="col-img grid-vertical-container">
+                <div class="cell-image"><img :src="getPkmnImagePath()" width="105px" height="105px" alt="pokemon image"></div>
+                <div class="cell-species cell-center">Lv: {{ enemy.value.level }}</div>
+            </div>
+            <div class="col-stats grid-vertical-container">
+                <div class="cell-species cell-center">{{ enemy.value.species }}</div>
+                <table width="275px">
+                    <tr>
+                        <td class="cell-stats-enemy cell-center cell-atk">{{ enemy.value.attack }}</td>
+                        <td class="cell-stats-enemy cell-center cell-def">{{ enemy.value.defense }}</td>
+                        <td class="cell-stats-enemy cell-center cell-spa">{{ enemy.value.specialAttack }}</td>
+                        <td class="cell-stats-enemy cell-center cell-spd">{{ enemy.value.specialDefense }}</td>
+                        <td class="cell-stats-enemy cell-center cell-spe">{{ enemy.value.speed }}</td>
+                    </tr>
+                </table>
+                <table width="275px">
+                    <tr>
+                        <td class="cell-center">{{ enemy.value.move1 }}</td>
+                        <td class="cell-center">{{ enemy.value.move2.toString() === null ? '-' : enemy.value.move2.toString() }}</td>
+                    </tr>
+                    <tr>
+                        <td class="cell-center">{{ enemy.value.move3.toString() === null ? '-' : enemy.value.move3.toString() }}</td>
+                        <td class="cell-center">{{ enemy.value.move4.toString() === null ? '-' : enemy.value.move4.toString() }}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
   `,
 });
